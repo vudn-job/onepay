08fe09736dd3a07d30b61a9979c59eb9c8e5b673
DAO Nguyen Vu <vudn.job@gmail.com>
2015-09-23 10:48:32 +0700

    First commit

---
+++ src/OnePay.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vudn
+ * Date: 9/23/15
+ * Time: 10:42 AM
+ */
+
+namespace Vudn\Onepay;
+
+class OnePay
+{
+    public static function test()
+    {
+        return 'Hello World, Composer!';
+    }
+}
